d0f418a09179a47305fd39fa151243207ec6c96a
Steven Rouk <stevenrouk@gmail.com>
2019-09-09 15:40:35 -0600

    Test loading 1000 rows into graph data structure.

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ if __name__ == "__main__":
     g_directed_class.add_edge('C', 'F')
     g_directed_class.remove_edge('F', 'C')
 
-    # Read in 10 lines of Reddit Hyperlink data.
-    data_loader = DataLoader(cols_to_load=['SOURCE_SUBREDDIT', 'TARGET_SUBREDDIT'])
+    # Read in some lines of Reddit Hyperlink data.
+    # The code as it is currently written takes a while to load for 1000 lines.
+    # The efficiency could be immediately improved through changing lists to sets in the classes,
+    # where appropriate.
+    data_loader = DataLoader(num_lines=1000, cols_to_load=['SOURCE_SUBREDDIT', 'TARGET_SUBREDDIT'])
     lines = data_loader.load()
+
+    reddit_body_hyperlink_graph = DirectedGraph({})
+    for line in lines[1:]:
+        #print("Working on {}".format(line))
+        if line[0] not in reddit_body_hyperlink_graph.nodes:
+            #print("ADD NODE: Adding node {} with edge {}".format(line[0], line[1]))
+            reddit_body_hyperlink_graph.add_node(line[0], [line[1]])
+        elif line[1] not in reddit_body_hyperlink_graph.graph[line[0]]:
+            #print("ADD EDGE: Adding edge {} to node {}".format(line[1], line[0]))
+            reddit_body_hyperlink_graph.add_edge(line[0], line[1])
+        else:
+            #print("TODO: ABILITY TO ADD MULTIPLE EDGES")
+            #TODO - We want to add multiple edges from one node to another eventually.
+            pass
+        #input()
