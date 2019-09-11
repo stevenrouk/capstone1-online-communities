1d9503c9f9388ad5ed4e184c5d7f44d204ec76b8
Steven Rouk <stevenrouk@gmail.com>
2019-09-10 19:35:55 -0600

    Add helper functions to color graph by node degree.

--- src/pyvis_helpers.py
+++ src/pyvis_helpers.py
@@ -1,3 +1,6 @@
+import random
+import numpy as np
+
 import networkx as nx
 from pyvis.network import Network
 
@@ -13,6 +16,43 @@ def create_example_viz(filename):
     graph = nx.davis_southern_women_graph()
     create_viz_html(graph, filename)
 
+def create_example_viz_2(filename):
+    g = Network()
+    g.add_nodes(
+        [1,2,3],
+        value=[10, 100, 400],
+        title=["I am node 1", "node 2 here", "and im node 3"],
+        x=[21.4, 54.2, 11.2], y=[100.2, 23.54, 32.1],
+        label=["NODE 1", "NODE 2", "NODE 3"],
+        color=["#00ff1e", "#162347", "#dd4b39"]
+    )
+    g.show(filename)
+
+def visualize_random_graph(filename, color_by_degree=True):
+    # Create a random graph.
+    G = nx.fast_gnp_random_graph(20, 0.5, seed=42, directed=True)
+    g = Network()
+    g.barnes_hut()
+    G_nodes = G.nodes
+
+    # Coloring by degree:
+    if color_by_degree:
+        G_degrees = [G.degree(node) for node in G_nodes]
+        G_colors = nums_to_greyscale_hex(G_degrees)
+
+        for node, color in zip(G_nodes, G_colors):
+            g.add_node(node, color=color)
+    
+    # Not coloring by degree:
+    else:
+        for node in G_nodes:
+            g.add_node(node)
+    
+    for edge in G.edges:
+        g.add_edge(*edge)
+
+    g.show(filename)
+
 def create_viz_html(graph, filename):
     filename = ensure_html_filename(filename)
 
@@ -20,3 +60,30 @@ def create_viz_html(graph, filename):
     g_vis.barnes_hut()
     g_vis.from_nx(graph)
     g_vis.show(filename)
+
+def random_color():
+    r = lambda: random.randint(0,255)
+    return '#%02X%02X%02X' % (r(),r(),r())
+
+def rgb_to_hex(rgb_triplet):
+    return '#%02x%02x%02x' % rgb_triplet
+
+def nums_to_greyscale_hex(nums, how='linear'):
+    """Maps a list of numbers to gradiented red hex colors.
+    
+    Standardizes all values from 0 to 1 then maps values to
+    shades of red based on node degree, with greater degrees
+    mapping to brighter red.
+    """
+    nums = np.array(nums)
+    if how == 'log':
+        nums = np.log(nums)
+    max_nums = max(nums)
+    min_nums = min(nums)
+    nums_range = max_nums - min_nums
+    nums_scaled = (nums - min_nums) / nums_range * 255
+    nums_scaled = nums_scaled.astype(int)
+    rgb_vals = [(v, 20, 20) for v in nums_scaled]
+    hex_vals = [rgb_to_hex(trip) for trip in rgb_vals]
+
+    return hex_vals
