d72372bf49aaed36611acaf5a07d3e9951c8a54a
Steven Rouk <stevenrouk@gmail.com>
2019-09-13 08:52:01 -0600

    Add docstrings.

--- src/networkx_helpers.py
+++ src/networkx_helpers.py
@@ -4,18 +4,70 @@ from collections import Counter
 import networkx as nx
 
 def max_n_out_degree(graph, n):
+    """Finds the top-n nodes by out-degree in a graph.
+
+    Parameters
+    ----------
+    graph : NetworkX graph object
+    n : int
+        The number of nodes to return
+
+    Returns
+    -------
+    list
+        A list of n nodes from the graph with the top out-degrees.
+    """
     out_degrees = graph.out_degree()
     return sorted(out_degrees, key=lambda x: x[1], reverse=True)[:n]
 
 def max_n_in_degree(graph, n):
+    """Finds the top-n nodes by in-degree in a graph.
+
+    Parameters
+    ----------
+    graph : NetworkX graph object
+    n : int
+        The number of nodes to return
+
+    Returns
+    -------
+    list
+        A list of n nodes from the graph with the top in-degrees.
+    """
     in_degrees = graph.in_degree()
     return sorted(in_degrees, key=lambda x: x[1], reverse=True)[:n]
 
 def max_n_degree(graph, n):
+    """Finds the top-n nodes by total degree in a graph.
+
+    Parameters
+    ----------
+    graph : NetworkX graph object
+    n : int
+        The number of nodes to return
+
+    Returns
+    -------
+    list
+        A list of n nodes from the graph with the top total degrees.
+    """
     degree = graph.degree()
     return sorted(degree, key=lambda x: x[1], reverse=True)[:n]
 
 def combine_graphs(graph1, graph2):
+    """Takes two NetworkX graphs and returns a combined graph with all
+    nodes and edges from each graph.
+
+    Parameters
+    ----------
+    graph1 : NetworkX graph object
+    graph2 : NetworkX graph object
+
+    Returns
+    -------
+    NetworkX graph object
+        A graph with all nodes and edges from both graphs
+    """
     combined_graph = copy.deepcopy(graph1)
     combined_graph.add_nodes_from(graph2)
     combined_graph.add_edges_from(graph2.edges)
@@ -23,12 +75,36 @@ def combine_graphs(graph1, graph2):
     return combined_graph
 
 def top_n_pagerank(graph, n):
+    """Finds the top-n nodes by PageRank in a graph.
+
+    Parameters
+    ----------
+    graph : NetworkX graph object
+    n : int
+        The number of nodes to return
+
+    Returns
+    -------
+    list
+        A list of n nodes from the graph with the top PageRank.
+    """
     pr = nx.pagerank(graph)
     pr_items = list(pr.items())
     
     return sorted(pr_items, key=lambda x: x[1], reverse=True)[:n]
 
-def degree_of_connected_components(graph):
+def get_component_length_counts(graph):
+    """Returns the counts of the lengths (i.e. number of nodes) of each component.
+
+    Parameters
+    ----------
+    graph : NetworkX graph object
+
+    Returns
+    -------
+    Counter
+        A Counter object with the counts of the lengths of the components
+    """
     graph_undirected = graph.to_undirected()
     connected_components_list = list(nx.connected_components(graph_undirected))
     
